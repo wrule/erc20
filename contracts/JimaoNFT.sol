1a5a5dc35a07148ce8dc6f43dd0c57ae576a2fd6
jimao <jimao@jimaodeMacBook-Pro.local>
2022-01-19 14:11:39 +0800

    阅读erc721

--- contracts/JimaoNFT.sol
+++ contracts/JimaoNFT.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract JimaoNFT {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract JimaoNFT is ERC721 {
+  constructor()
+  ERC721("Jimao", "JMT") { }
+
   event Log(string);
 
   function hello() public {
