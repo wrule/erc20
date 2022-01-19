205bb56d4eecbcaa0770311a30436a764933110b
jimao <jimao@jimaodeMacBook-Pro.local>
2022-01-19 09:56:01 +0800

    建立合约

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const JimaoNFT = artifacts.require("JimaoNFT");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(JimaoNFT);
 };
