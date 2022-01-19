94524678c830257a01e4942a9835346e4c2e42e5
jimao <jimao@jimaodeMacBook-Pro.local>
2022-01-19 16:57:38 +0800

    JimaoNFT

--- contracts/JimaoNFT.sol
+++ contracts/JimaoNFT.sol
@@ -1,15 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 
-contract JimaoNFT is ERC721 {
+contract JimaoNFT is ERC721PresetMinterPauserAutoId {
   constructor()
-  ERC721("Jimao", "JMT") { }
-
-  event Log(string);
-
-  function hello() public {
-    emit Log("hello world");
-  }
+  ERC721PresetMinterPauserAutoId("Jimao", "JMT", "https://www.jimaojin.xyz/nft/")
+  { }
 }
