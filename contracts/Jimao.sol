fafffde4b07c9448aa22406c43dae3fc54693146
jimao <jimao@jimaodeMacBook-Pro.local>
2022-01-21 14:34:36 +0800

    erc777

--- contracts/Jimao.sol
+++ contracts/Jimao.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Jimao {
+import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 
+contract Jimao is ERC777 {
+  constructor(
+    uint256 initialSupply,
+    address[] memory defaultOperators
+  )
+  ERC777("JimaoToken", "Jimao", defaultOperators) {
+    _mint(msg.sender, initialSupply, "", "");
+  }
 }
