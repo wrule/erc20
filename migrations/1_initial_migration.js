d82cfdce847085f4eb47173b6b180e0ced7d8cb6
jimao <jimao@jimaodeMacBook-Pro.local>
2022-01-21 16:11:52 +0800

    migrate

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 const Jimao = artifacts.require("Jimao");
 
-module.exports = function (deployer) {
-  deployer.deploy(Jimao);
+module.exports = function (deployer, network, accounts) {
+  console.log('>>>', accounts.length);
+  deployer.deploy(Jimao, 1000, accounts);
 };
